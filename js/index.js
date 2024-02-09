92ef99f77f5f6d0915995281d5e2873a3aed4bed
Sergio gomar <sergio.gomar04@gmail.com>
2024-02-09 01:06:49 -0600

    Añadiendo boton para vender

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 let cash = _cashier(db_cash);
 let products = _products(db_product);
 let purchase = _purchase(db_purchase);
+let sales = _sales(db_sales)
 
 const getCash = () => {
     let cashText = document.getElementById("cash");
@@ -34,16 +35,72 @@ const buildTable = () => {
 
     products.getAllProduct().forEach((element) => {
         let row = document.createElement("tr");
+        const sellButton = `sale-${element}`;
+        const purchaseButton = `purchase-${element}`;
+
         row.innerHTML = `
             <th scope="row">${element.id}</th>
             <td>${element.name}</td>
             <td>${element.stored}</td>
             <td>${element.price}</td>
-            <td></td>
-            <td></td>
+            <td><button type="button" class="btn btn-primary" id="${sellButton}">Vender</button></td>
+            <td><button type="button" class="btn btn-primary" id="${purchaseButton}">Comprar</button></td>
         `;
         lista.appendChild(row);
+
+        document.getElementById(sellButton).addEventListener('click', (e) => {
+            sellButtonEvent(element.id)
+        });
+        document.getElementById(purchaseButton).addEventListener('click', );
     });
+}
+
+const sellButtonEvent = (id) => {
+    let container = document.getElementById("sellContainer");
+    let producto = products.getProduct(id);
+
+    container.innerHTML = `
+        <div class="col">
+            <h5>${producto.name}</h5>
+        </div>
+        <div class="col">
+            <h6>Existe: ${producto.stored} kg</h6>
+        </div>
+        <div class="col">
+            <label for"sellItem">Cantidad a vender (kg)</label>
+            <input type="text" class="form-control" id="sellItem">
+            <button type="button" class="btn btn-success" id"btnSellItem">Vender</button>
+        </div>
+        <div class="col">
+            <button type="button" class="btn btn-danger" id="cancelSell">Cancelar</button>
+        </div>
+    `;
+    document.getElementById('btnSellItem').addEventListener('click', (e) => {
+        const amount = document.getElementById("sellItem").value;
+        sellProductAction(producto, new Number(amount))
+    })
+
+    document.getElementById("cancelSell").addEventListener("click", cancelOp);
+}
+
+const sellProductAction = (producto, amount) => {
+    try{
+        const totalSale = amount * producto.price;
+        producto.sale(producto.id, amount);
+        cash.sale(totalSale);
+        sales.new(producto.id, amount, producto.price);
+    }
+    catch(err){
+        alert(err.error);
+    }
+
+    buildTable();
+    getCash();
+    cancelOp();
+}
+
+const cancelOp = () => {
+    document.getElementById("sellContainer").innerHTML="";
 }
 
 const submitEvent = (e) => {
@@ -73,4 +130,5 @@ const addProductEventEnd = () => {
 addEventListener('load', getCash);
 addEventListener('load', buildTable);
 addEventListener('submit', submitEvent);
-document.getElementById('addProductBtn').addEventListener('click', addProductEventStart)
+document.getElementById('addProductBtn').addEventListener('click', addProductEventStart);
+document.getElementById('addProductBtn').addEventListener('click', addProductEventEnd);
